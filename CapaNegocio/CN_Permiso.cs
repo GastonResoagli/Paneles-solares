9f79a0971407148c73031690fceb6c0e42de4e83
Matias Lago <lagomatias01@hotmail.com>
2025-09-22 16:20:38 -0300

    varios

--- CapaNegocio/CN_Permiso.cs
+++ CapaNegocio/CN_Permiso.cs
@@ -9,10 +9,13 @@ using CapaEntidad;
 
 namespace CapaNegocio
 {
+    //clase de la capa negocio que maneja los permisos
     public class CN_Permiso
     {
+        //objeto de la capa datos, se conecta a la base de datos
         private CD_Permiso objcd_permiso = new CD_Permiso();
 
+        //metodo que lista los permisos de un usuario
         public List<Permiso> Listar(int idusuario)
         {
             return objcd_permiso.Listar(idusuario);
